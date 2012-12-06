a44a8445abc120947c093d2fa5b4aee70fa9728e
unknown <Attila@Attila-PC.(none)>
2012-12-06 21:58:51 +0100

    UTF-8, HU

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ $app_name = idx($app_info, 'name', '');
 
 ?>
 <!DOCTYPE html>
-<html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
+<html xmlns:fb="http://ogp.me/ns/fb#" lang="hu">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
